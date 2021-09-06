0e29187e342137b22a5b725b3f1adfe36e2c45c2
Lucas <83249829+lucas112525@users.noreply.github.com>
2021-09-06 09:06:38 -0300

    Delete disney directory

--- disney/src/main/java/com/dis/disney/repository/PeliculaSerieRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dis.disney.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.dis.disney.model.PeliculaSerie;
-
-@Repository
-public interface PeliculaSerieRepository extends JpaRepository<PeliculaSerie, Long>{
-
-}
